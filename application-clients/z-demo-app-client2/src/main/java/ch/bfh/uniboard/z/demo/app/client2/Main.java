7be58aaa06e2c8ac2cbd3b426382893a4d318f75
sevhau <severin.hauser@bfh.ch>
2013-11-21 13:21:18 +0100

    ssl

--- application-clients/z-demo-app-client2/src/main/java/ch/bfh/uniboard/z/demo/app/client2/Main.java
+++ application-clients/z-demo-app-client2/src/main/java/ch/bfh/uniboard/z/demo/app/client2/Main.java
@@ -9,9 +9,9 @@ import javax.naming.InitialContext;
  * Hello world!
  *
  */
-public class Main
-{
- 	public static void main(String[] args) throws Exception {
+public class Main {
+
+	public static void main(String[] args) throws Exception {
 		System.out.println("Starting Z Demo Application Client 1...");
 
 		DemoAccessRemote svc = lookupService();
@@ -21,16 +21,21 @@ public class Main
 	}
 
 	private static DemoAccessRemote lookupService() throws Exception {
-        Properties p = new Properties();
-        p.put("org.omg.CORBA.ORBInitialHost", "localhost");
-        p.put("org.omg.CORBA.ORBInitialPort", "3700");
-        p.put("java.naming.factory.initial", "com.sun.enterprise.naming.SerialInitContextFactory");
-        p.put("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
-        p.put("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+		Properties props = System.getProperties();
+
+		props.put("javax.net.ssl.trustStore", "/keystore.jks");
+		props.put("javax.net.ssl.trustStorePassword", "changeit");
+
+		Properties p = new Properties();
+		p.put("org.omg.CORBA.ORBInitialHost", "ziu");
+		p.put("org.omg.CORBA.ORBInitialPort", "3700");
+		p.put(Context.SECURITY_PROTOCOL, "ssl");
+		p.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
+		p.put(Context.URL_PKG_PREFIXES, "com.sun.enterprise.naming");
+		p.put(Context.STATE_FACTORIES, "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
 
 		Context ctx = new InitialContext(p);
-		DemoAccessRemote svc = (DemoAccessRemote)
-            ctx.lookup("java:global/z-demo/z-demo-access-1.0-SNAPSHOT/DemoAccessBean");
+		DemoAccessRemote svc = (DemoAccessRemote) ctx.lookup("java:global/z-demo/z-demo-access-1.0-SNAPSHOT/DemoAccessBean");
 		return svc;
 	}
 }
