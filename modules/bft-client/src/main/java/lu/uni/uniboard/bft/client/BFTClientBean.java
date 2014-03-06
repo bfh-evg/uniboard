47fdbd69134188229eca25bc51cb696add162744
Eric Dubuis <eric.dubuis@bfh.ch>
2014-03-06 09:10:39 +0100

    Added TODO marker.

--- modules/bft-client/src/main/java/lu/uni/uniboard/bft/client/BFTClientBean.java
+++ modules/bft-client/src/main/java/lu/uni/uniboard/bft/client/BFTClientBean.java
@@ -1,9 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2014 ... Rui?
+ *
+ * Project VIVO.
+ *
+ * Distributable under GPL license.
+ * See terms of license at gnu.org.
  */
-
 package lu.uni.uniboard.bft.client;
 
 import javax.ejb.Local;
@@ -12,9 +14,11 @@ import lu.uni.uniboard.bft.service.BFTClient;
 import lu.uni.uniboard.bft.service.BFTServiceMessage;
 
 /**
- * This class is the entry point for BFT protocol messages at the BFT 
+ * This class is the entry point for BFT protocol messages at the BFT
  * client side.
- * 
+ * <p>
+ * TODO: Envision to inherit from "ch.bfh.uniboard.service.Component".
+ *
  * @author Rui Joaquim
  */
 @Stateless
@@ -24,13 +28,13 @@ public class BFTClientBean implements BFTClient{
     /**
      * This method receives a BFT protocol message to be processed by the
      * BFT client
-     * 
-     * @param bftMessage the message to process 
+     *
+     * @param bftMessage the message to process
      */
     @Override
     public void processMessage(BFTServiceMessage bftMessage) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
+
 }
