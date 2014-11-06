05fb152852ed2e166fcbc2411513800f76ac4b4e
sevhau <severin.hauser@bfh.ch>
2014-11-06 15:50:04 +0100

    Integrated RestService

--- modules/bfh-restservice/src/main/java/ch/bfh/uniboard/restservice/UniBoardRestService.java
+++ modules/bfh-restservice/src/main/java/ch/bfh/uniboard/restservice/UniBoardRestService.java
@@ -28,25 +28,10 @@ import javax.ws.rs.core.MediaType;
 @Path("")
 public interface UniBoardRestService {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	protected static final Logger logger = Logger.getLogger(UniBoardRestService.class.getName());
 
-	@EJB
-	private GetService getService;
-
-	@EJB
-	private PostService postService;
-=======
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public QueryDTO get();
->>>>>>> 3c43d0662f3fe3c12031f15f9081deabb9a95c10
-=======
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public QueryDTO get();
->>>>>>> 3c43d0662f3fe3c12031f15f9081deabb9a95c10
 
 	@POST
 	@Path("query")
@@ -58,26 +43,6 @@ public interface UniBoardRestService {
 	@Path("post")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public AttributesDTO post(PostContainerDTO postContainer) {
-		try {
-			byte[] message = DatatypeConverter.parseBase64Binary(postContainer.getMessage());
-			Attributes alpha = Transformer.convertAttributesDTOtoAttributes(postContainer.getAlpha());
-			Attributes beta = new Attributes();
-			logger.log(Level.INFO, "Post message={0}, alpha={1}, beta={2}", 
-					new Object[]{DatatypeConverter.printBase64Binary(message), alpha, beta});
-			beta = postService.post(message, alpha, beta);
-			return Transformer.convertAttributesToDTO(beta);
-		} catch (TransformException ex) {
-			logger.log(Level.SEVERE, null, ex);
-			throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
-		}
-	}
-=======
 	public AttributesDTO post(PostContainerDTO postContainer);
->>>>>>> 3c43d0662f3fe3c12031f15f9081deabb9a95c10
-=======
-	public AttributesDTO post(PostContainerDTO postContainer);
->>>>>>> 3c43d0662f3fe3c12031f15f9081deabb9a95c10
+
 }
