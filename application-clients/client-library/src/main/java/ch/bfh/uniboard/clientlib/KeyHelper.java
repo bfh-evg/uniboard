1680e021612b6441732b54d8404f098c1c3a0043
sevhau <severin.hauser@bfh.ch>
2015-06-15 14:42:16 +0200

    Removed catch for Algorithm

--- application-clients/client-library/src/main/java/ch/bfh/uniboard/clientlib/KeyHelper.java
+++ application-clients/client-library/src/main/java/ch/bfh/uniboard/clientlib/KeyHelper.java
@@ -23,8 +23,6 @@ import java.security.spec.DSAPublicKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Helper class allowing to transform BigInteger keys into PublicKey and PrivateKey objects
@@ -33,97 +31,79 @@ import java.util.logging.Logger;
  */
 public class KeyHelper {
 
-    private static final Logger logger = Logger.getLogger(PostHelper.class.getName());
+	/**
+	 * Transform a BigInteger DSA private key into a DSAPrivateKey object
+	 *
+	 * @param p value of prime p
+	 * @param q value of prime q
+	 * @param g value of generator g
+	 * @param x value of private key x
+	 * @return the corresponding DSAPrivateKey object, null if an error occured
+	 * @throws InvalidKeySpecException if values do not represent a valid private key
+	 * @throws java.security.NoSuchAlgorithmException if the DSA algorithm is not supported
+	 */
+	public static DSAPrivateKey createDSAPrivateKey(BigInteger p, BigInteger q, BigInteger g, BigInteger x) throws
+			InvalidKeySpecException, NoSuchAlgorithmException {
 
-    /**
-     * Transform a BigInteger DSA private key into a DSAPrivateKey object
-     *
-     * @param p value of prime p
-     * @param q value of prime q
-     * @param g value of generator g
-     * @param x value of private key x
-     * @return the corresponding DSAPrivateKey object, null if an error occured
-     * @throws InvalidKeySpecException if values do not represent a valid private key
-     */
-    public static DSAPrivateKey createDSAPrivateKey(BigInteger p, BigInteger q, BigInteger g, BigInteger x) throws
-	    InvalidKeySpecException {
+		DSAPrivateKeySpec keySpec = new DSAPrivateKeySpec(x, p, q, g);
+		KeyFactory keyFactory = KeyFactory.getInstance("DSA");
 
-	DSAPrivateKeySpec keySpec = new DSAPrivateKeySpec(x, p, q, g);
-	KeyFactory keyFactory;
-	try {
-	    keyFactory = KeyFactory.getInstance("DSA");
-	} catch (NoSuchAlgorithmException ex) {
-	    logger.log(Level.SEVERE, "Invalid key algorithm given.", ex);
-	    return null;
+		return (DSAPrivateKey) keyFactory.generatePrivate(keySpec);
 	}
-	return (DSAPrivateKey) keyFactory.generatePrivate(keySpec);
-    }
 
-    /**
-     * Transform a BigInteger DSA public key into a DSAPublicKey object
-     *
-     * @param p value of prime p
-     * @param q value of prime q
-     * @param g value of generator g
-     * @param y value of public key y
-     * @return the corresponding DSAPublicKey object, null if an error occured
-     * @throws InvalidKeySpecException if values do not represent a valid public key
-     */
-    public static DSAPublicKey createDSAPublicKey(BigInteger p, BigInteger q, BigInteger g, BigInteger y) throws
-	    InvalidKeySpecException {
+	/**
+	 * Transform a BigInteger DSA public key into a DSAPublicKey object
+	 *
+	 * @param p value of prime p
+	 * @param q value of prime q
+	 * @param g value of generator g
+	 * @param y value of public key y
+	 * @return the corresponding DSAPublicKey object, null if an error occured
+	 * @throws InvalidKeySpecException if values do not represent a valid public key
+	 * @throws java.security.NoSuchAlgorithmException if the DSA algorithm is not supported
+	 */
+	public static DSAPublicKey createDSAPublicKey(BigInteger p, BigInteger q, BigInteger g, BigInteger y) throws
+			InvalidKeySpecException, NoSuchAlgorithmException {
 
-	DSAPublicKeySpec keySpec = new DSAPublicKeySpec(y, p, q, g);
-	KeyFactory keyFactory;
-	try {
-	    keyFactory = KeyFactory.getInstance("DSA");
-	} catch (NoSuchAlgorithmException ex) {
-	    logger.log(Level.SEVERE, "Invalid key algorithm given.", ex);
-	    return null;
+		DSAPublicKeySpec keySpec = new DSAPublicKeySpec(y, p, q, g);
+		KeyFactory keyFactory = KeyFactory.getInstance("DSA");
+
+		return (DSAPublicKey) keyFactory.generatePublic(keySpec);
 	}
-	return (DSAPublicKey) keyFactory.generatePublic(keySpec);
-    }
 
-    /**
-     * Transform a BigInteger RSA private key into a RSAPrivateKey object
-     *
-     * @param modulus value of RSA modulus
-     * @param d value of private key
-     * @return the corresponding RSAPrivateKey object, null if an error occured
-     * @throws InvalidKeySpecException if values do not represent a valid private key
-     */
-    public static RSAPrivateKey createRSAPrivateKey(BigInteger modulus, BigInteger d) throws
-	    InvalidKeySpecException {
+	/**
+	 * Transform a BigInteger RSA private key into a RSAPrivateKey object
+	 *
+	 * @param modulus value of RSA modulus
+	 * @param d value of private key
+	 * @return the corresponding RSAPrivateKey object, null if an error occured
+	 * @throws InvalidKeySpecException if values do not represent a valid private key
+	 * @throws java.security.NoSuchAlgorithmException if the RSA algorithm is not supported
+	 */
+	public static RSAPrivateKey createRSAPrivateKey(BigInteger modulus, BigInteger d) throws
+			InvalidKeySpecException, NoSuchAlgorithmException {
 
-	RSAPrivateKeySpec spec = new RSAPrivateKeySpec(modulus, d);
-	KeyFactory keyFactory;
-	try {
-	    keyFactory = KeyFactory.getInstance("RSA");
-	} catch (NoSuchAlgorithmException ex) {
-	    logger.log(Level.SEVERE, "Invalid key algorithm given.", ex);
-	    return null;
+		RSAPrivateKeySpec spec = new RSAPrivateKeySpec(modulus, d);
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+		return (RSAPrivateKey) keyFactory.generatePrivate(spec);
 	}
-	return (RSAPrivateKey) keyFactory.generatePrivate(spec);
-    }
 
-    /**
-     * Transform a BigInteger RSA public key into a RSAPublicKey object
-     *
-     * @param modulus value of RSA modulus
-     * @param e value of public key
-     * @return the corresponding RSAPublicKey object, null if an error occured
-     * @throws InvalidKeySpecException if values do not represent a valid public key
-     */
-    public static RSAPublicKey createRSAPublicKey(BigInteger modulus, BigInteger e) throws
-	    InvalidKeySpecException {
+	/**
+	 * Transform a BigInteger RSA public key into a RSAPublicKey object
+	 *
+	 * @param modulus value of RSA modulus
+	 * @param e value of public key
+	 * @return the corresponding RSAPublicKey object, null if an error occured
+	 * @throws InvalidKeySpecException if values do not represent a valid public key
+	 * @throws java.security.NoSuchAlgorithmException if the RSA algorithm is not supported
+	 */
+	public static RSAPublicKey createRSAPublicKey(BigInteger modulus, BigInteger e) throws
+			InvalidKeySpecException, NoSuchAlgorithmException {
 
-	RSAPublicKeySpec spec = new RSAPublicKeySpec(modulus, e);
-	KeyFactory keyFactory;
-	try {
-	    keyFactory = KeyFactory.getInstance("RSA");
-	} catch (NoSuchAlgorithmException ex) {
-	    logger.log(Level.SEVERE, "Invalid key algorithm given.", ex);
-	    return null;
+		RSAPublicKeySpec spec = new RSAPublicKeySpec(modulus, e);
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+		return (RSAPublicKey) keyFactory.generatePublic(spec);
 	}
-	return (RSAPublicKey) keyFactory.generatePublic(spec);
-    }
 }
